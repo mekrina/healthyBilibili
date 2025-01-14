aa59269f235057b9698feacdab43334da1276dd5
freestyle <2023302181261@whu.edu.cn>
2025-01-14 20:53:09 +0800

    version 1.1.0

--- hello.js
+++
@@ -1,7 +0,0 @@
-const likeNum = document.querySelector(".video-like-info.video-toolbar-item-text") || document.querySelector(".info-text");
-if (!likeNum) {
-  console.error("likeNum not found");
-}
-new MutationObserver(() => {
-  console.log("hello world");
-}).observe(likeNum, {childList: true, subtree: true, characterData: true});
